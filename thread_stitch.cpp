81eeefcfbf1f83611caa897ebf64dbb50d8d8b0d
Chia-ChunHsu <hsucc6351518@gmail.com>
2015-08-30 21:40:34 +0800

    Version 1.0.4 Almost Fix the composition problem

--- thread_stitch.cpp
+++ thread_stitch.cpp
@@ -19,13 +19,11 @@ int Thread_Stitch::Warp(std::vector<cv::Mat> &CalMat,std::vector<cv::Mat> &CalWa
 
     if (cv::Stitch::OK == status)
     {
-        //cv::imshow("result",Result);
         return 1;
     }
     else
     {
         return 0;
-        //qDebug()<<"Stitching fail.";
     }
 }
 
